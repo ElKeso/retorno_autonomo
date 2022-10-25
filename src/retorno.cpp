56546ce205747c4b52e9683c8587460e1cb0f59f
ElKeso <115960132+ElKeso@users.noreply.github.com>
2022-10-25 15:19:43 -0300

    Update retorno.cpp

--- src/retorno.cpp
+++ src/retorno.cpp
@@ -1,13 +1,13 @@
 #include "ros/ros.h"
-#include "retorno_autonomo/ret.h"
-#include "retorno_autonomo/tray.h"
-#include "retorno_autonomo/trayArray.h"
+//#include "retorno_autonomo/ret.h"
+//#include "retorno_autonomo/tray.h"
+//#include "retorno_autonomo/trayArray.h"
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/Twist.h"
 #include "math.h"
 
-    retorno_autonomo::trayArray camino;
-    retorno_autonomo::tray datos;
+    //retorno_autonomo::trayArray camino;
+    //retorno_autonomo::tray datos;
     nav_msgs::Odometry cam;
     geometry_msgs::Twist mover;
     int f=0; float cte=1.5;
@@ -17,16 +17,16 @@
     int i=0; int c; int c_p;
     float x; float y;
 
-void retorno(const retorno_autonomo::ret &r){
-    c=r.datos;
-    c=c-2;
-    c_p=c/10;
-    f=r.ret;
-}
+//void retorno(const retorno_autonomo::ret &r){
+//    c=r.datos;
+//    c=c-2;
+//    c_p=c/10;
+//    f=r.ret;
+//}
 
-void trayectoria(const retorno_autonomo::trayArray &t){
-  camino=t;
-}
+//void trayectoria(const retorno_autonomo::trayArray &t){
+//  camino=t;
+//}
 
 void posicion(const nav_msgs::Odometry &pos){
     cam=pos;
@@ -58,16 +58,16 @@ int main(int argc, char **argv){
   ros::Rate loop_rate(10000);
   ros::Rate rate(1);
   c=0;
-  datos.x=c;
-  datos.y=c;
-  datos.theta=c;
-  camino.trayectoria.push_back(datos);
+  //datos.x=c;
+  //datos.y=c;
+  //datos.theta=c;
+  //camino.trayectoria.push_back(datos);
   while(ros::ok()){
     ros::spinOnce();
     //moverse a las cordenadas...
     if(f==1){
-    x=camino.trayectoria[c].x;
-    y=camino.trayectoria[c].y;
+    //x=camino.trayectoria[c].x;
+    //y=camino.trayectoria[c].y;
       if(abs(conv(eu_angular(x, y))-cam.pose.pose.orientation.z)>t_a){
         mover.linear.x=0;
         if(eu_angular(x, y)<0){
